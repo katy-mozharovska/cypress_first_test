d4fec9fab534ad290f978f240b06da062d5e93c3
Katy Mozharovska <katy.mozharovska@gmail.com>
2022-01-25 11:52:10 +0200

    Solution

---
+++ cypress/integration/my_first_cypress_test.js
@@ -0,0 +1,20 @@
+//<reference types="cypress" />
+
+describe('Your Second Test', () => {
+  let user;
+
+  before(function () {
+    cy.task("freshUser").then((freshUser) => {
+        user = freshUser;
+    });
+  });
+
+  it('Sign up with faker', () => {
+    cy.visit('https://react-redux.realworld.io/#/register?_k=luy0fu');
+    cy.get(':nth-child(1) > .form-control').type(user.username).should('have.value', user.username);
+    cy.get(':nth-child(2) > .form-control').type(user.email).should('have.value', user.email);
+    cy.get(':nth-child(3) > .form-control').type(user.password).should('have.value', user.password);
+    cy.get('.btn').click();
+    cy.get(':nth-child(4) > .nav-link').should('contain', user.username);
+  })
+})
